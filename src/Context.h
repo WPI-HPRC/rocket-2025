5c1ddcb9fff3635c42c5c962dfa9acb2317471b7
coburndaniel979 <coburndaniel979@gmail.com>
2025-03-20 17:45:49 -0400

    changes

--- src/Context.h
+++ src/Context.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include "boilerplate/Sensors/Impl/ASM330.h"
+#include "boilerplate/Sensors/Impl/ICM20948.h"
 #include "boilerplate/Sensors/Impl/LPS22.h"
+
 struct Context {
+  ASM330* accel;
   Barometer* baro;
+  ICM20948* icm;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 #include <Arduino.h>
 
 #include "Context.h"
+#include "boilerplate/Sensors/Impl/ASM330.h"
+#include "boilerplate/Sensors/Impl/ICM20948.h"
+#include "boilerplate/Sensors/Impl/LPS22.h"
 #include "states/States.h"
 #include <boilerplate/Sensors/SensorManager/SensorManager.h>
 #include <boilerplate/StateMachine/StateMachine.h>
 
 Context ctx = {
+    .accel = new ASM330(),
     .baro = new Barometer(),
+    .icm = new ICM20948(),
 };
 
-Sensor *sensors[1] = {ctx.baro};
+Sensor *sensors[3] = {ctx.accel, ctx.baro, ctx.icm};
+
 SensorManager<decltype(&millis), 1> sensorManager(sensors, millis);
 
 StateMachine stateMachine((State *)new PreLaunch(&ctx));
