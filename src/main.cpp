ee146519b64b7e9b12ff89d8b38244a96bd7244b
Dashiell Elliott <dashiell.elliott@yahoo.com>
2025-03-24 18:32:19 -0400

    first attempt to make board config

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,31 @@
 #include <boilerplate/Sensors/SensorManager/SensorManager.h>
 #include <boilerplate/StateMachine/StateMachine.h>
 
-Context ctx = {
-    .accel = new ASM330(),
-    .baro = new Barometer(),
-    .icm = new ICM20948(),
-    .max10s = new MAX10S(),
-};
+// Context ctx = {
+//     .accel = new ASM330(),
+//     .baro = new Barometer(),
+//     .icm = new ICM20948(),
+//     .max10s = new MAX10S(),
+// };
 
-Sensor *sensors[] = {ctx.accel, ctx.baro, ctx.icm};
+// Sensor *sensors[] = {ctx.accel, ctx.baro, ctx.icm};
 
-SensorManager<decltype(&millis), sizeof(sensors)/sizeof(Sensor*)> sensorManager(sensors, millis);
+// SensorManager<decltype(&millis), sizeof(sensors)/sizeof(Sensor*)> sensorManager(sensors, millis);
 
-StateMachine stateMachine((State *)new PreLaunch(&ctx));
+// StateMachine stateMachine((State *)new PreLaunch(&ctx));
 
 void setup() {
     Serial.begin(9600);
+    pinMode(PB9, OUTPUT);
+    return;
     Wire.begin();
-    stateMachine.initialize();
-    sensorManager.sensorInit();
+    // stateMachine.initialize();
+    // sensorManager.sensorInit();
 }
 
 void loop() {
-    stateMachine.loop();
-    sensorManager.loop();
+    digitalWrite(PB9, 1);
+    return;
+    // stateMachine.loop();
+    // sensorManager.loop();
 }
