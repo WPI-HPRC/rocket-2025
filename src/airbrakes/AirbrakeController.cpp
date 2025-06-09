41e12f4badcfd437b654422e7dc7729b834e9f70
coburndaniel979 <coburndaniel979@gmail.com>
2025-06-09 01:23:33 -0400

    compute logic

--- src/airbrakes/AirbrakeController.cpp
+++ src/airbrakes/AirbrakeController.cpp
@@ -12,3 +12,44 @@ void AirbrakeController::write(int val) {
 int AirbrakeController::read() {
   return analogRead(feedback_pin);
 }
+
+float AirbrakeController::deployAmmount(float acc_z, float v_z, float alt, float curr_deploy) {
+  acc_z = (acc_z - 1) * g * -1;
+
+  float curr_max_height = alt - (v_z*v_z)/(2*acc_z);
+  if(curr_max_height <= target_height) {
+    return 0;
+  } else {
+    float new_deploy = curr_deploy * 1.05;
+    return (new_deploy >= 1 ? 1 : new_deploy); 
+  }
+}
+
+float AirbrakeController::controllBreak() {
+  const auto icmData = magData.getData();
+  const auto accelData = accData.getData();
+  const auto lpsData = baroData.getData();
+
+  float acc_z_best = magData.accelZ;
+  float alt_best = baroData.altitude;
+  // populate in case no new data to use
+
+  if(accelData.getLastUpdated() != lastAccRead) {
+    acc_z_best = accelData.accelZ;
+    lastAccRead = accelData.getLastUpdated();
+  }
+
+  // more accurate suposedly so better to use
+  if(magData.getLastUpdated() != lastMagRead) {
+    acc_z_best = magData.accelZ;
+    lastMagRead = magData.getLastUpdated();
+  }
+
+  if(baroData.getLastUpdated() != lastMagRead) {
+    alt_best = baroData.altitude;
+    lastBaroRead = baroData.getLastUpdated();
+  }
+
+  return deployAmmount(acc_z_best, float v_z, alt_best, current_break_deploy);
+  // still need to get v_z from somewhere
+}
--- src/airbrakes/AirbrakeController.h
+++ src/airbrakes/AirbrakeController.h
@@ -1,13 +1,21 @@
 #pragma once
 
+#include "../boilerplate/Sensors/Impl/ASM330.h"
+#include "../boilerplate/Sensors/Impl/ICM20948.h"
+#include "../boilerplate/Sensors/Impl/LPS22.h"
+#include "../boilerplate/TimedPointer/TimedPointer.h"
 #include <Arduino.h>
 #include <Servo.h>
 #include <cstdint>
 
 class AirbrakeController {
   public:
-    AirbrakeController(uint8_t servo_pin, uint8_t feedback_pin)
-        : servo_pin(servo_pin), feedback_pin(feedback_pin), servo() {}
+    AirbrakeController(uint8_t servo_pin, uint8_t feedback_pin,
+                       const TimedPointer<ICMData> magData,
+                       const TimedPointer<ASM330Data> accData,
+                       const TimedPointer<LPS22Data> baroData)
+        : servo_pin(servo_pin), feedback_pin(feedback_pin), servo(),
+          magData(magData), accData(accData), baroData(baroData) {}
 
     void init();
 
@@ -15,8 +23,25 @@ class AirbrakeController {
 
     int read();
 
+    float controllBreak();
+
   private:
+    float deployAmmount(float acc_z, float v_z, float alt, float curr_deploy);
+    // acc_z = (acc_z - g)*-g
+    // target_height = alt - (v_z)^2/2acc_z_gloal
+
     uint8_t servo_pin;
     uint8_t feedback_pin;
     Servo servo;
+    float init_break_deploy;
+    float current_break_deploy;
+    float target_height = 3073.0; // m. This is 25m over actual target to
+                                  // account for most likley going under target?
+                                  // think this over
+    const TimedPointer<ICMData> magData;
+    const TimedPointer<ASM330Data> accData;
+    const TimedPointer<LPS22Data> baroData;
+    long lastMagRead = 0; 
+    long lastAccRead = 0;
+    long lastBaroRead = 0;
 };
